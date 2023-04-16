80dc9f21ad0681c0ad8184dd955bc5e20fb749f9
stevnzhang <roboticdraqo@gmail.com>
2023-04-16 14:09:33 -0400

    Added preliminary frontend for detailed task view and archived todo-list view

--- EXP-Client/App.js
+++ EXP-Client/App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import TaskItem from './TaskItem'
+import ArchivedDay from './ArchivedDay'
 
 export default function App() {
+  const task = {
+    name: 'Finish Project',
+    deadline: new Date('2023-05-01').toLocaleDateString(),
+    completed: false,
+    rating: 4
+  };
+
+  const tasks = [
+    { name: 'Task 1', deadline: 'April 20, 2023', completed: true },
+    { name: 'Task 2', deadline: 'April 22, 2023', completed: false },
+    { name: 'Task 3', deadline: 'April 21, 2023', completed: false },
+    { name: 'Task 4', deadline: 'April 22, 2023', completed: true },
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <TaskItem task={task} /> */}
+      {/* <ArchivedDay tasks={tasks} /> */}
     </View>
   );
 }
