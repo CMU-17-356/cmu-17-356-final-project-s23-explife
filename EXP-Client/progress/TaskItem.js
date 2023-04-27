d159c0108afe4edb0f83dca87ad61e390317b940
Emma Jiang <31319479+emmayjiang@users.noreply.github.com>
2023-04-26 21:42:42 -0400

    implement deletion

--- EXP-Client/progress/TaskItem.js
+++ EXP-Client/progress/TaskItem.js
@@ -1,38 +1,44 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Button, StyleSheet, Modal, Pressable } from 'react-native';
 import { IconButton } from 'react-native-paper';
 import { Rating } from 'react-native-ratings';
 import EditTaskMenu from '../todo/EditTaskMenu.js'
 
-import Dialog, { DialogFooter, DialogButton, DialogContent } from 'react-native-popup-dialog';
-
 function DeleteItem({ task, isDeleting, setIsDeleting, setViewingTask }) {
 
-  const onDelete = (name) => {
+  const onDelete = () => {
     setIsDeleting(false);
-    console.log(`Deleted ${name}`)
+    console.log(`Deleted ${task.name}`)
+    setViewingTask(null);
   };
 
   return (
-    <Dialog
+    <Modal
+      animationType="slide"
+      transparent={true}
       visible={isDeleting}
-      footer={
-        <DialogFooter>
-          <DialogButton
-            text="CANCEL"
-            onPress={() => setIsDeleting(false)}
-          />
-          <DialogButton
-            text="OK"
-            onPress={() => { onDelete(task) }}
-          />
-        </DialogFooter>
-      }
-    >
-      <DialogContent>
-        <Text>Are you sure you want to delete this item?</Text>
-      </DialogContent>
-    </Dialog>
+      onRequestClose={() => {
+        Alert.alert('Modal has been closed.');
+        setIsDeleting(false);
+      }}>
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <Text style={styles.modalText}>Are you sure you want to delete this item?</Text>
+          <View style={styles.buttons}>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setIsDeleting(false)}>
+              <Text style={styles.textStyle}>Cancel</Text>
+            </Pressable>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={onDelete}>
+              <Text style={styles.textStyle}>Yes</Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    </Modal>
   )
 }
 
@@ -115,4 +121,46 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    padding: 10,
+    elevation: 2,
+    width: 100,
+  },
+  buttonClose: {
+    backgroundColor: '#B1B1B1',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  buttons: {
+    flexDirection: 'row',
+    columnGap: 50,
+  }
 });
