27634973d37ab52afb9567d509c5a147666fbbce
Anushehzm <anushehz@gmail.com>
2023-05-02 18:17:40 -0400

    Font size bug fix

--- EXP-Client/stories/StoryView.js
+++ EXP-Client/stories/StoryView.js
@@ -48,16 +48,10 @@ const styles = StyleSheet.create({
   header: {
     padding: 20
   },
-  title: {
-    fontWeight: "bold",
-    fontSize: "1.5rem",
-    marginVertical: "1em",
-    textAlign: "center"
-  },
   text: {
-    lineHeight: "1.5em",
-    fontSize: "1.125rem",
-    marginVertical: "1em",
+    lineHeight: 16,
+    fontSize: 20,
+    marginVertical: 16,
     textAlign: "center"
   },
   link: {
